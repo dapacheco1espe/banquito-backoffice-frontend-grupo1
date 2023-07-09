cc6f60aa75dfc1fa774923cd9b6b0c5342e36c57
Alexander Pacheco <dapacheco1@espe.edu.ec>
2023-07-08 19:32:25 -0500

    feat(default menu): default menu items for template

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -12,11 +12,44 @@ export const defaultNavigation: FuseNavigationItem[] = [
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id      : 'dashboards',
+        title   : 'Dashboards',
+        tooltip : 'Dashboards',
+        type    : 'aside',
+        icon    : 'heroicons_outline:home',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'apps',
+        title   : 'Apps',
+        tooltip : 'Apps',
+        type    : 'aside',
+        icon    : 'heroicons_outline:qrcode',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'pages',
+        title   : 'Pages',
+        tooltip : 'Pages',
+        type    : 'aside',
+        icon    : 'heroicons_outline:document-duplicate',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'user-interface',
+        title   : 'UI',
+        tooltip : 'UI',
+        type    : 'aside',
+        icon    : 'heroicons_outline:collection',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id      : 'navigation-features',
+        title   : 'Navigation',
+        tooltip : 'Navigation',
+        type    : 'aside',
+        icon    : 'heroicons_outline:menu',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
     }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
