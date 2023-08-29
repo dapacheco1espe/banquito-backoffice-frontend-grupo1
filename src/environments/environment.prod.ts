57fe2e92b4a2e289ddd34c0e73b6a72b55ad7319
MaverickPaucar <mrpaucar@espe.edu.ec>
2023-08-28 19:12:35 -0500

    upd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
-    apiResrURL: 'https://prueba-lhnfqyqejq-uc.a.run.app',
-    administrationAPIUrl: 'https://api18-62gh6lrj.uc.gateway.dev',
+    apiResrURL: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app/api',
+    administrationAPIUrl:
+        'https://banquito-ws-administracion2-zsorzzk5da-uc.a.run.app/',
     apiURLClient: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app/api',
     apiURLAccounts:
         'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api',
