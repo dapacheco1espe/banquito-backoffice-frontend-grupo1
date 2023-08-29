3a4ddc0786ae48fba662f65664275653567e96c9
MaverickPaucar <mrpaucar@espe.edu.ec>
2023-08-28 19:02:36 -0500

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false,
-    apiResrURL: 'https://banquito-ws-client-production.up.railway.app',
+    production: true,
+    apiResrURL: 'https://prueba-lhnfqyqejq-uc.a.run.app',
+    administrationAPIUrl:
+        'https://banquito-ws-administracion2-zsorzzk5da-uc.a.run.app/',
     apiURLClient: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app/api',
     apiURLAccounts:
         'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api',
-    // administrationAPIUrl:
-    //     'https://banquito-ws-administration-production-6806.up.railway.app',
-    administrationAPIUrl:
-        'https://banquito-ws-administracion2-zsorzzk5da-uc.a.run.app/',
-    // administrationAPIUrl: 'https://api18-62gh6lrj.uc.gateway.dev',
-    // apiKey: '?key=AIzaSyDMQIDdAhNpwjoovbocJ1xJfzzMZZqGmFY',
 };
 
 /*
