bcdc3b8fd5106314a4281d345d05866767059cb4
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-28 19:34:11 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiResrURL: 'https://banquito-ws-client-production.up.railway.app',
+    apiResrURL: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app',
     apiURLClient: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app/api',
     apiURLAccounts: 'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api',
     // administrationAPIUrl:
