7ebcaee0a0a5fa0aa23a55178e40b73a63b2cc95
MaverickPaucar <mrpaucar@espe.edu.ec>
2023-08-31 21:29:09 -0500

    links de api gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
 export const environment = {
-    production: false,
-    apiResrURL: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app',
-    apiURLClient: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app/api',
-    apiURLAccounts: 'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api',
-    // administrationAPIUrl:
-    //     'https://banquito-ws-administration-production-6806.up.railway.app',
-    administrationAPIUrl:
-        'https://banquito-ws-administracion2-zsorzzk5da-uc.a.run.app/',
+    production: true,
+    apiURLClient: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    apiResrURL: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    administrationAPIUrl: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,10 @@
 
 export const environment = {
     production: false,
-    apiURLClient: 'https://banquito-ws-client-zsorzzk5da-uc.a.run.app/api',
-    apiURLAccounts: 'https://banquito-accounts-try5-zsorzzk5da-uc.a.run.app/api',
-    // administrationAPIUrl:
-    //     'https://banquito-ws-administration-production-6806.up.railway.app',
-    apiResrURL: 'https://prueba-lhnfqyqejq-uc.a.run.app',
-    administrationAPIUrl:
-        'https://banquito-ws-administracion2-zsorzzk5da-uc.a.run.app/',
+    apiURLClient: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    apiResrURL: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    administrationAPIUrl: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
 };
 
 /*
