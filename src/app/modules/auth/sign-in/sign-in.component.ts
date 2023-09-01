245dbf16bda7afd83b94605193160a226b3f7184
Danny Pacheco <135442481+dapacheco1espe@users.noreply.github.com>
2023-09-01 16:07:57 -0500

    change validation fields name

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -98,7 +98,7 @@ export class AuthSignInComponent implements OnInit
                     // Set the alert
                     this.alert = {
                         type   : 'error',
-                        message: 'Wrong email or password'
+                        message: 'Correo o contraseña incorrecta'
                     };
 
                     // Show the alert
