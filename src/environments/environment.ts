a9640b2b102decf4339235329b2661e2100ca055
Danny Pacheco <135442481+dapacheco1espe@users.noreply.github.com>
2023-09-01 02:44:02 -0500

    fix environments vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,13 @@
 
 export const environment = {
     production: false,
-    apiURLClient: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
-    apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    apiURLClient: 'https://arquitectura-1sa89r3l.uc.gateway.dev/api',
+    apiURLAccounts: 'https://arquitectura-1sa89r3l.uc.gateway.dev/api',
+    // administrationAPIUrl:
+    //     'https://banquito-ws-administration-production-6806.up.railway.app',
     apiResrURL: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
-    administrationAPIUrl: 'https://arquitectura-1sa89r3l.uc.gateway.dev',
+    administrationAPIUrl:
+        'https://arquitectura-1sa89r3l.uc.gateway.dev/',
     apiURLproductsPassive: 'https://arquitectura-1sa89r3l.uc.gateway.dev/api',
 };
 
